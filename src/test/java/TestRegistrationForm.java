f584a66c680e96e62bd55c03a1bd6621d91f487a
dark <lultrs@mail.ru>
2022-03-30 17:46:37 +0300

    added registration form testing

---
+++ src/test/java/TestRegistrationForm.java
@@ -0,0 +1,30 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestRegistrationForm {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void testRegistrationForm(){
+        String firstName = "joka";
+        String lastName = "boka";
+        String email = "test@qa.qa";
+
+        open("https://demoqa.com/automation-practice-form");
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("[for=gender-radio-1]").click();
+
+
+    }
+}
